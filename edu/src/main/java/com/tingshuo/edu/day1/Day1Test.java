15fbf6305e08eb2868ce2c1024ea7fa49150c6cf
yangzwt <yangzwt@163.com>
2026-02-27 19:18:51 +0800

    基础学习开始

---
+++ edu/src/main/java/com/tingshuo/edu/day1/Day1Test.java
@@ -0,0 +1,10 @@
+package com.tingshuo.edu.day1;
+
+/**
+ * 第一天学习测试类
+ */
+public class Day1Test {
+    public static void main(String[] args) {
+        System.out.println("开始学习第一天学习"+"我开始了");
+    }
+}
