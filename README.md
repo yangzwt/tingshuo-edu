bef9ce39df2ae04121fd44fbce7a11649940c0d4
yangzwt <yangzwt@163.com>
2026-02-27 20:16:52 +0800

    基础学习开始02

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tingshuo-edu
 个人基础学习资料管理
+# Java 学习进度
+
+## Day 1 - Hello World
+- 学习了 Java 环境配置
+- 写出了第一个程序
+
+## Day 2 - 变量定义
+- 学习了 int, double, String, boolean 类型
+- 能够定义和修改变量
+- 成功输出个人信息
+
+## 下一步计划
+- 学习运算符
+- 学习条件判断
---
+++ edu/src/main/java/com/tingshuo/edu/day1/VariableTest.java
@@ -0,0 +1,28 @@
+package com.tingshuo.edu.day1;
+
+/**
+ * @description: ai辅助学习
+ * @author: yangZwt
+ * @date: 2026/2/27 20:02
+ * @version: 1.0
+ */
+public class VariableTest {
+    public static void main(String[] args) {
+        // 定义 整数变量年龄18
+        int age=20;
+        // 定义一个小数类型，表示身高
+        double height =1.85;
+        // 定义一个字符串，表示名字
+        String name="张小明";
+        // 定义一个布尔类型，表示是否是学生
+        boolean isStudent=false;
+        // 定义一个小数类型，表示体重
+        double weight=60.5;
+
+        System.out.println("我的名字是："+name);
+        System.out.println("我今年："+age+"岁");
+        System.out.println("我的身高是："+height+"米");
+        System.out.println("我是学生吗？"+isStudent);
+        System.out.println("我的体重是？"+weight+"公斤");
+    }
+}
