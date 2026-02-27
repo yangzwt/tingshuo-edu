3f816365bf1722de78d20ba02b7ee8632a56016c
yangzwt <yangzwt@163.com>
2026-02-27 19:53:59 +0800

    基础学习开始01

---
+++ edu/src/main/java/com/tingshuo/edu/day1/HelloWord.java
@@ -0,0 +1,13 @@
+package com.tingshuo.edu.day1;
+
+/**
+ * @description: 基础学习
+ * @author: yangZwt
+ * @date: 2026/2/27 19:47
+ * @version: 1.0
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
