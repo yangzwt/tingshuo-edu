f036165f65612386d81783987fe76a68f982a8cd
yangzwt <yangzwt@163.com>
2026-03-01 00:30:53 +0800

    基础学习第三天运算符的使用

---
+++ edu/src/main/java/com/tingshuo/edu/day3/VariableDay3.java
@@ -0,0 +1,17 @@
+package com.tingshuo.edu.day3;
+
+/**
+ * @description: java程序运算符
+ * @author: yangZwt
+ * @date: 2026/3/1 0:01
+ * @version: 1.0
+ */
+public class VariableDay3 {
+    public static void main(String[] args) {
+        // 写一个程序计算“买3个苹果，每个5元，一共多少钱”
+        int apple=3;
+        double dj=5.0;
+        double sum =apple*dj;
+        System.out.println("一共花了多少钱"+sum+"元");
+    }
+}
