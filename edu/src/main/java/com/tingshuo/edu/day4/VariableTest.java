9627696d5a9ef2e4cf36a640eb3592ea913f60d9
yangzwt <yangzwt@163.com>
2026-03-02 22:35:21 +0800

    基础学习if和else-if和else提交

--- edu/src/main/java/com/tingshuo/edu/day4/VariableTest.java
+++ edu/src/main/java/com/tingshuo/edu/day4/VariableTest.java
@@ -16,11 +16,25 @@ public class VariableTest {
         Scanner scanner =new Scanner(System.in);
         System.out.println("请您输入您的成绩，我来告诉您是否及格。");
         int score = scanner.nextInt();//输入的成绩
-        if (score>60){
-            System.out.println("您好您的成绩是及格的。不要骄傲哦！！");
-        }else {
-            System.out.println("您的成绩不合格，请继续努力！！");
-        }
-
+//        if (score>60){
+//            System.out.println("您好您的成绩是及格的。不要骄傲哦！！");
+//        }else {
+//            System.out.println("您的成绩不合格，请继续努力！！");
+//        }
+        //45 60 80 90 从小到达排序
+        // 90 80 60 45
+        // 1.校验
+        // 2.注意 不能先判断数据小，即成绩 不能判断小的
+       if ( score <0 || score >100){
+           System.out.println("您好您输入的成绩不在指定范围内，请检查后输入。");
+       } else if (score>=90) {
+           System.out.println("您好您的成绩是优秀的。继续保持哦！！");
+       } else if (score >=80) {
+           System.out.println("您好您的成绩是良好的。继续加油哦！！");
+       }else if (score >= 60){
+           System.out.println("您好您的成绩是及格的。不要骄傲哦！！");
+       }else {
+           System.out.println("您的成绩不合格，请继续努力！！");
+       }
     }
 }
