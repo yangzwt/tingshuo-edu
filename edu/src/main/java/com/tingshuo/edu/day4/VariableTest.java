9b947c418c742e494d94bcc0e49c3f8236d61719
yangzwt <yangzwt@163.com>
2026-03-02 20:24:59 +0800

    基础学习if和else分支比较

---
+++ edu/src/main/java/com/tingshuo/edu/day4/VariableTest.java
@@ -0,0 +1,26 @@
+package com.tingshuo.edu.day4;
+
+import java.util.Scanner;
+
+/**
+ * @description: java学习if和else
+ * @author: yangZwt
+ * @date: 2026/3/2 19:15
+ * @version: 1.0
+ */
+public class VariableTest {
+    // 需求是键盘输入数据，计算您的成绩是否及格是否是优秀，如果分数大于60，打印及格，否则打印不及格,score成绩，
+    // 增加一个条件，如果大于90分，打印“优秀”，如果小于60认为不及格
+    // 使用if 和 else 进行比较
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("请您输入您的成绩，我来告诉您是否及格。");
+        int score = scanner.nextInt();//输入的成绩
+        if (score>60){
+            System.out.println("您好您的成绩是及格的。不要骄傲哦！！");
+        }else {
+            System.out.println("您的成绩不合格，请继续努力！！");
+        }
+
+    }
+}
